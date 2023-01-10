473079f46e4266eac25a7f73093c6cde5113cdc5
ndandrea44 <121039766+Neal-dandrea@users.noreply.github.com>
2023-01-10 23:23:15 +0000

    Just added some comments

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-workout = input("how many times do you workout per week?")
-print(workout)
+workout = input("how many times do you workout per week? ")
+print("you workout " + workout + " times per week")
+
+## generate a workout/split analyzer
+## A user will input their split and an analysis of the split will be conducted that determines if the optimal amount of rest is taken in between workouts 
+## an optimal split is hitting each muscle group consistently without overworking and underworking each group, I must determine what it means to "overwork" and "underwork" each group.
+## for quads, hamstrings, and glutes it should be 2 times a week
+## for chest, shoulders, neck, and upper/lower back it should be 2 times a week
+## for biceps, triceps, calves, and forearms it should be 3-4 times per week
+## minimum effective volume and maximum adaptive volume and maximum adaptive volume 
+## 
+
+
+
+
+
+
